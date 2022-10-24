adc579ae0d552a435b00d616ad6c626963ed02ca
Alexandra Kwon <alexandra.kwon1@gmail.com>
2022-10-23 17:14:45 -0700

    Adding WiFi code

---
+++ wifi/wifi.ino
@@ -0,0 +1,17 @@
+/*
+  Code to connect the Nano 33 IoT to a Wi-Fi network
+
+  Adapted from Connecting the Nano 33 IoT to a Wi-Fi Network by Fabricio Troya
+
+  by Alexandra Kwon (UID: 205290932) for ECE M119
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
